ce9bd0b02e6a5fe5475b4a6630b8164e864b48b6
Tokita <yamashitakazuo09@gmail.com>
2023-09-27 17:56:36 +0100

    project initialization

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const app = express();
+
+const port = 4000;
+
+// Define routes and middleware here
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
