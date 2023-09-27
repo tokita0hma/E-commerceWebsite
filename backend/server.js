442cd7161bffad7a78e72da23e8dcf292dff6819
Tokita <yamashitakazuo09@gmail.com>
2023-09-27 18:03:30 +0100

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
-
 const express = require('express');
 const app = express();
 
+//remember to add thos to an env file so we can decentrilize the settings
 const port = 4000;
 
 // Define routes and middleware here
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
